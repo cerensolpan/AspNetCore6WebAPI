d98a4d4bbf8d8b14c83333332d1cc86352b5f9f5
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2023-08-10 20:15:40 +0300

    feat: Create getCities on controller

---
+++ AspNetCore6WebAPI/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore6WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+    }
+}
+
