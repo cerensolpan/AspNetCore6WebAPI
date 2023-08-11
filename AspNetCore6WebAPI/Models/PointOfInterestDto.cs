e38e75753491bd4f18d1aa19174f8f74402e466e
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2023-08-11 11:51:02 +0300

    feat: Create point of interests model

---
+++ AspNetCore6WebAPI/Models/PointOfInterestDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AspNetCore6WebAPI.Controllers
+{
+	public class PointOfInterestDto
+	{
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
+
