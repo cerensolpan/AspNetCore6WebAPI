d8a59383dc5d28da1cecb7a0692cc2bfb0a6afa1
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2023-08-11 11:50:30 +0300

    feat: Add status for get city

--- AspNetCore6WebAPI/Controllers/CitiesController.cs
+++ AspNetCore6WebAPI/Controllers/CitiesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCore6WebAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,9 +13,22 @@ namespace AspNetCore6WebAPI.Controllers
     public class CitiesController : ControllerBase
     {
         [HttpGet()]
-        public JsonResult GetCities()
+        public ActionResult<IEnumerable<CityDto>> GetCities()
         {
-            return new JsonResult(CitiesDataStore.Current.Cities);
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity( int id)
+        {
+            var result = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == id);
+
+            if(result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
         }
     }
 }
