28e3c8621529a9fe1662e106879f3248ac0ec598
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2023-08-11 11:52:31 +0300

    feat: Create points of interest controller

--- AspNetCore6WebAPI/CitiesDataStore.cs
+++ AspNetCore6WebAPI/CitiesDataStore.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AspNetCore6WebAPI.Controllers;
 using AspNetCore6WebAPI.Models;
 
 namespace AspNetCore6WebAPI
@@ -16,18 +17,51 @@ namespace AspNetCore6WebAPI
                      Id = 1,
                      Name = "New York City",
                      Description = "The one with that big park.",
+                     PointsOfInterest = new List<PointOfInterestDto>()
+                     {
+                         new PointOfInterestDto() {
+                             Id = 1,
+                             Name = "Central Park",
+                             Description = "The most visited urban park in the United States." },
+                          new PointOfInterestDto() {
+                             Id = 2,
+                             Name = "Empire State Building",
+                             Description = "A 102-story skyscraper located in Midtown Manhattan." },
+                     }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Antwerp",
                     Description = "The one with the cathedral that was never really finished.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                     {
+                         new PointOfInterestDto() {
+                             Id = 3,
+                             Name = "Cathedral of Our Lady",
+                             Description = "A Gothic style cathedral, conceived by architects Jan and Pieter Appelmans." },
+                          new PointOfInterestDto() {
+                             Id = 4,
+                             Name = "Antwerp Central Station",
+                             Description = "The the finest example of railway architecture in Belgium." },
+                     }
                 },
                 new CityDto()
                 {
                     Id= 3,
                     Name = "Paris",
                     Description = "The one with that big tower.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                     {
+                         new PointOfInterestDto() {
+                             Id = 5,
+                             Name = "Eiffel Tower",
+                             Description = "A wrought iron lattice tower on the Champ de Mars, named after engineer Gustave Eiffel." },
+                          new PointOfInterestDto() {
+                             Id = 6,
+                             Name = "The Louvre",
+                             Description = "The world's largest museum." },
+                     }
                 },
             };
 
---
+++ AspNetCore6WebAPI/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore6WebAPI.Controllers
+{
+	[Route("api/cities/{cityId}/pointsofinterest")]
+	[ApiController]
+	public class PointsOfInterestController : ControllerBase
+	{
+		[HttpGet()]
+		public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+		{
+			var city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+
+			if(city == null)
+			{
+				return NotFound();
+			}
+			return Ok(city.PointsOfInterest);
+		}
+        [HttpGet("{pointofinterestid}")]
+		public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
+		{
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+			var pointOfInterest = city.PointsOfInterest.FirstOrDefault(point => point.Id == pointOfInterestId);
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+			return Ok(pointOfInterest);
+        }
+
+    }
+}
+
