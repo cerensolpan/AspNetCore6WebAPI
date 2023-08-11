f3009ea201622e658e3743f94dedd7326aae5477
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2023-08-11 15:55:59 +0300

    feat: Create create point of interest post request

--- AspNetCore6WebAPI/Controllers/PointsOfInterestController.cs
+++ AspNetCore6WebAPI/Controllers/PointsOfInterestController.cs
@@ -18,7 +18,8 @@ namespace AspNetCore6WebAPI.Controllers
 			}
 			return Ok(city.PointsOfInterest);
 		}
-        [HttpGet("{pointofinterestid}")]
+
+        [HttpGet("{pointofinterestid}",Name = "GetPointOfInterest")]
 		public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
 		{
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
@@ -33,6 +34,34 @@ namespace AspNetCore6WebAPI.Controllers
             }
 			return Ok(pointOfInterest);
         }
+
+		[HttpPost]
+		public ActionResult<PointOfInterestDto> CreatePointOfInterest(int cityId,PointOfInterestForCreationDto pointOfInterest)
+		{
+			var city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+			if(city == null)
+			{
+				return NotFound();
+			}
+
+			var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(city => city.PointsOfInterest).Max(p => p.Id);
+			var finalPointOfInterest = new PointOfInterestDto()
+			{
+				Id = ++maxPointOfInterestId,
+				Name = pointOfInterest.Name,
+				Description = pointOfInterest.Description
+			};
+
+			city.PointsOfInterest.Add(finalPointOfInterest);
+
+			return CreatedAtRoute("GetPointOfInterest",
+				new
+				{
+					cityId = cityId,
+					pointOfInterestId = finalPointOfInterest.Id
+				},
+				finalPointOfInterest);
+		}
 
     }
 }
---
+++ AspNetCore6WebAPI/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCore6WebAPI.Controllers
+{
+	public class PointOfInterestForCreationDto
+	{
+        [Required(ErrorMessage = "You should provide a name value")]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+    }
+}
+
