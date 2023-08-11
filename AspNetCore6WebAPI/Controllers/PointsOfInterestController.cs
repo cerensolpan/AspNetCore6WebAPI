d4aff0546017c5dafbf8b21055c61b77d20a8d91
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2023-08-11 16:18:56 +0300

    feat: Create update point of interest put request

--- AspNetCore6WebAPI/Controllers/PointsOfInterestController.cs
+++ AspNetCore6WebAPI/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCore6WebAPI.Controllers
@@ -62,6 +63,29 @@ namespace AspNetCore6WebAPI.Controllers
 				},
 				finalPointOfInterest);
 		}
+
+		[HttpPut("{pointofinterestid}")]
+
+		public ActionResult UpdatePointOfInterest (int cityId, int pointOfInterestId, PointOfInterestForUpdateDto pointOfInterest)
+		{
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+			var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(c => c.Id == pointOfInterestId);
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+			pointOfInterestFromStore.Name = pointOfInterest.Name;
+			pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+			return NoContent();
+
+        }
 
     }
 }
