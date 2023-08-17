a91f74b55210fc18b8a26ed541b774762eef6093
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2023-08-17 15:33:35 +0300

    feat: Create cloud mail service

---
+++ AspNetCore6WebAPI/Services/CloudMailService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace AspNetCore6WebAPI.Services
+{
+	public class CloudMailService : IMailService
+    {
+        private readonly string _mailTo = string.Empty;
+        private readonly string _mailFrom = string.Empty;
+
+        public CloudMailService(IConfiguration configuration)
+        {
+            _mailTo = configuration["mailSettings:mailToAddress"];
+            _mailFrom = configuration["mailSettings:mailFromAddress"];
+        }
+
+        public void Send(string subject, string message)
+        {
+            Console.WriteLine($"Mail from {_mailFrom} ton {_mailTo}, " + $"with {nameof(CloudMailService)}");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine($"Subject: {message}");
+        }
+    }
+}
+
