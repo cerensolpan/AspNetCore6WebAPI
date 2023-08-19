14faab7cbb31aa1da3cad9c066bcec72588728b7
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2023-08-19 17:37:50 +0300

    feat: Create city and point of interest entities

---
+++ AspNetCore6WebAPI/Entities/PointOfInterest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspNetCore6WebAPI.Entities
+{
+	public class PointOfInterest
+	{
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [ForeignKey("CityId")]
+        public City? City { get; set; }
+        public int CityId { get; set; }
+
+        public PointOfInterest(string name)
+        {
+            Name = name;
+        }
+    }
+}
+
