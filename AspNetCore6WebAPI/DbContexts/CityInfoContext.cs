f6c802e01b703f60a153df78cce566db9bc3be2a
Ceren Şolpan Türe <73752127+cerensolpan@users.noreply.github.com>
2023-08-23 13:08:06 +0300

    feat: Create city info context

---
+++ AspNetCore6WebAPI/DbContexts/CityInfoContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AspNetCore6WebAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCore6WebAPI.DbContexts
+{
+	public class CityInfoContext : DbContext
+	{
+		public DbSet<City> Cities { get; set; } = null!;
+		public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("connectionstring");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
